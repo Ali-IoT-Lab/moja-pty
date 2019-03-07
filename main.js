96c764045bfc39f0bdca62c074d7b96641cca3fa
Mountains-and-rivers <156110386@qq.com>
2019-03-07 17:19:21 +0800

    1

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 var os = require('os');
+var moduleName = '';
+
+if (os.arch() == 'arm64'){
+  moduleName = 'node-pty';
+}else {
+  moduleName = 'node-pty-prebuilt';
+}
 
 var mojaPty = function () {
-if (os.arch() == 'arm64'){
-    return require('node-pty');
-  }else {
-    return require('node-pty-prebuilt');
-  }
+  return require(moduleName);
 };
-
 
 module.exports = {
   "moja_pty" : mojaPty
