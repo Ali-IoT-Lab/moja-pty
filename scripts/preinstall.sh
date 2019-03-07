85b05c9cea3a84bcb7f627256c756b3766767af9
Mountains-and-rivers <156110386@qq.com>
2019-03-07 18:16:02 +0800

    1

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -9,13 +9,13 @@ isArmV8=$( echo $cpuType | grep "aarch" )
 
 if [ -n "${isX86}" ] ; then
   npm install node-pty-prebuilt@latest
-  echo "module.exports ={moduleName:`node-pty-prebuilt`}" > $basepath/config.js
+  echo "module.exports ={moduleName:\"node-pty-prebuilt\"}" > $basepath/config.js
 elif [  -n "${isArm}" ] ;then
   npm install node-pty-prebuilt@latest
-  echo "module.exports ={moduleName:`node-pty-prebuilt`}" > $basepath/config.js
+  echo "module.exports ={moduleName:\"node-pty-prebuilt\"}" > $basepath/config.js
 elif [  -n "${isArmV8}" ] ;then
   npm install node-pty@latest
-  echo "module.exports ={moduleName:`node-pty`}" > $basepath/config.js
+  echo "module.exports ={moduleName:\"node-pty\"}" > $basepath/config.js
 else
   echo "--------------------------------------不支持的系统类型---------------------------------------"
   exit 1
